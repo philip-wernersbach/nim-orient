0dee7c3fd1a92ad4a2e140e48d8e7efaaf0c21d5
Philip Wernersbach <philip.wernersbach@gmail.com>
2015-06-23 16:39:19 -0400

    Support multibyte varints.

--- orient/src/orientpackets_unpack.nim
+++ orient/src/orientpackets_unpack.nim
@@ -6,6 +6,7 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import unsigned
+import macros
 from rawsockets import ntohs, ntohl
 
 import orienttypes
@@ -87,7 +88,45 @@ proc unpackString*(buffer: var OrientPacket, length: int): OrientString =
 # OrientDB's binary protocol uses varints + ZigZag encoding.
 # This saves a trivial amount of space but increases computational complexity, why oh why do they do this to us?
 
-# This could probably be done more efficiently with bit shifts, but unpackVarInt doesn't work for >1 byte anyways.
+# unpackVarIntLogic is used by unpackVarInt so that we don't have to repeat ourselves for each distinct
+# varint length (0-10). The macro generates code like this for a two byte varint:
+#
+# let part0: uint64 = (cast[ptr uint8](buffer.buffer))[] and 0x7F'u8
+# buffer += 1
+#
+# let part1: uint64 = (cast[ptr uint8](buffer.buffer))[]
+# buffer += 1
+#
+# return return OrientVarInt(unpackZigZagNum(part0 or (part1 shl 7)))
+#
+# We call this once for each distinct varint length.
+macro unpackVarIntLogic(ii: int): stmt =
+    var logic = ""
+
+    let i = intVal(ii) - 1
+
+    for j in countUp(0, i - 1):
+        logic &= "let part" & $j & ": uint64 = (cast[ptr uint8](buffer.buffer))[] and 0x7F'u8\n"
+        logic &= "buffer += 1\n\n"
+
+    logic &= "let part" & $i & ": uint64 = (cast[ptr uint8](buffer.buffer))[]\n"
+    logic &= "buffer += 1\n\n"
+
+    logic &= "return OrientVarInt(unpackZigZagNum(part0"
+
+    for j in countUp(1, i):
+        let shift = 7 * j
+        logic &= " or (part" & $j & " shl " & $shift & ")"
+
+    logic &= "))\n"
+
+    ## Uncomment this to see the code that this macro generates.
+    #debugEcho(logic)
+
+    return parseStmt(logic)
+
+
+# This could probably be done more efficiently with bit shifts.
 # TODO: Get someone that's smarter than me to implement this as bit shifts.
 proc unpackZigZagNum[T](data: T): T =
     if (data mod 2) != 0:
@@ -95,28 +134,44 @@ proc unpackZigZagNum[T](data: T): T =
     else:
         return data div 2
 
-# I can't get this to work for values larger than one byte, oh well.
-# TODO: Get someone that's smarter than me to make this work with >1 byte.
 proc unpackVarInt*(buffer: var OrientPacket): OrientVarInt =
-    var value: uint64 = 0
     var current: uint8
-    var shift_multiplier: uint64 = 0
+    var length = 0
 
-    while shift_multiplier < 7:
+    let oldCursor = buffer.cursor
+
+    while length <= 10:
         current = (cast[ptr uint8](buffer.buffer))[]
         buffer += 1
+        length += 1
 
-        value = value or (cast[uint64](current and cast[uint8](0x7F)) shl (shift_multiplier * 7))
-
-        if (current and cast[uint8](0x80)) == 0:
+        if (current and 0x80'u8) == 0:
             break
 
-        shift_multiplier += 1
+    buffer.cursor = oldCursor
 
-    case cast[int](shift_multiplier):
+    case length
     of 0:
-        return cast[OrientVarInt](unpackZigZagNum(cast[uint8](value)))
+        return 0
+    of 1:
+        unpackVarIntLogic(1)
+    of 2:
+        unpackVarIntLogic(2)
+    of 3:
+        unpackVarIntLogic(3)
+    of 4:
+        unpackVarIntLogic(4)
+    of 5:
+        unpackVarIntLogic(5)
+    of 6:
+        unpackVarIntLogic(6)
+    of 7:
+        unpackVarIntLogic(7)
+    of 8:
+        unpackVarIntLogic(8)
+    of 9:
+        unpackVarIntLogic(9)
+    of 10:
+        unpackVarIntLogic(10)
     else:
-        raise newException(OrientDBFeatureUnsupportedInLibrary, "This library only supports unpacking 8-bit varints!")
-
-    return 0
+        raise newException(OrientDBFeatureUnsupportedInLibrary, "This library only supports unpacking varints up to 64-bits in length!")
